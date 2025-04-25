a0e176517cd8c96748f141e486395d7341f561fe
riddler <eli.okumu@strathmore.edu>
2025-04-25 17:03:13 +0300

    Add files via upload

---
+++ third_question.py
@@ -0,0 +1,25 @@
+number = int(input("Enter a number: "))
+factorial = 1 #initializing the factorial variable to 1
+for i in range(1,number+1): #calculates the factorial using a for loop
+    factorial *=i
+print(factorial) #outputs the factorial of the number keyed in by the user
+
+
+
+
+
+
+
+
+
+
+
+
+
+#count = 0
+#while count <= number:
+ #   num = number * count
+  #  count += 1
+
+#print(num)
+
